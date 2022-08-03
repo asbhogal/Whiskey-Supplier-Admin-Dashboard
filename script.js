15511eadb54919e166b8f162762255a798cf4a68
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-08-03 15:43:20 +0100

    amendments

---
+++ script.js
@@ -0,0 +1,6 @@
+const body = document.querySelector("body"),
+const modeToggle = document.querySelector("body");
+
+modeToggle.addEventListener("click", () => {
+    body.classList.toggle("dark")
+});
